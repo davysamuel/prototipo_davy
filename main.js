9258c490cf641bc54dce7d45b5db9e60735d058a
davysamuel <129762408+davysamuel@users.noreply.github.com>
2024-04-12 12:27:54 -0300

    f

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ const tempoObjetivo1 = new Date ("2024-06-05T00:00:00");
 const tempoObjetivo2 = new Date ("2024-12-05T00:00:00");
 const tempoObjetivo3 = new Date ("2024-12-30T00:00:00");
 const tempoObjetivo4 = new Date ("2025-02-01T00:00:00");
-const tempoObjetivo5 = new Date ("2025-03-01T00:00:00");
+const tempoObjetivo5 = new Date ("2025-12-01T00:00:00");
 
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4,tempoObjetivo5 ];
